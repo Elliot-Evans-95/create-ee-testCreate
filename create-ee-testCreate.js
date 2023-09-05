65b8d8f87e6d2499ffa82efcc8f7f7d94de2ad69
Elliot Evans <ell15evans.nuls@googlemail.com>
2023-09-05 22:39:54 +0300

    feat(): mjs in dist folder

--- create-ee-testCreate.js
+++ create-ee-testCreate.js
@@ -1,1 +1,17 @@
-console.log('create-ee-testCreate');
+function create() {
+    console.log('creating');
+
+    return 'created!'
+}
+
+function eeCreate() {
+    console.log('eeCreate created');
+
+    return 'eeCreate created!'
+}
+
+create()
+
+export {
+    eeCreate
+}
